c353e947cdb7e3c5691e16bb25e62360c4211985
nllevin <50681582+nllevin@users.noreply.github.com>
2019-08-22 14:02:10 -0400

    Implement ::my_attr_accessor

--- build_your_own_active_record/lib/00_attr_accessor_object.rb
+++ build_your_own_active_record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,11 @@
+require 'byebug'
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      attr_str = "@#{ name }"
+      define_method(name) { instance_variable_get(attr_str) }
+      define_method(name.to_s + "=") { |val| instance_variable_set(attr_str, val) }
+    end
   end
 end
