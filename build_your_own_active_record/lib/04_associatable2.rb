752fbb7b082d0382bc6fa408f2e4e1a8915925ba
nllevin <50681582+nllevin@users.noreply.github.com>
2019-08-23 19:20:32 -0400

    Improve ::has_one_through to make only one query

--- build_your_own_active_record/lib/04_associatable2.rb
+++ build_your_own_active_record/lib/04_associatable2.rb
@@ -1,4 +1,5 @@
 require_relative '03_associatable'
+require 'byebug'
 
 # Phase IV
 module Associatable
@@ -6,16 +7,35 @@ module Associatable
 
   def has_one_through(name, through_name, source_name)
     through_options = self.assoc_options[through_name] 
-    through_class = through_options.model_class
-    
-    source_options = through_class.assoc_options[source_name]
 
+    through_class = through_options.model_class
+    through_table = through_options.table_name
+    through_primary_key = through_options.primary_key
+    through_foreign_key = through_options.foreign_key
+    
+    
     define_method(name) do
-      val_of_through_foreign_key = send(through_options.foreign_key)
-      through_instance = through_class.find(val_of_through_foreign_key)
+      source_options = through_class.assoc_options[source_name]
+
+      source_table = source_options.table_name
+      source_primary_key = source_options.primary_key
+      source_foreign_key = source_options.foreign_key
+      source_class = source_options.model_class
       
-      val_of_source_foreign_key = through_instance.send(source_options.foreign_key)
-      source_options.model_class.find(val_of_source_foreign_key)
+      results = DBConnection.execute(<<-SQL, send(through_foreign_key))
+        SELECT
+          #{source_table }.*
+        FROM
+          #{ source_table }
+        JOIN
+          #{ through_table }
+        ON 
+          #{ source_table }.#{ source_primary_key } = #{ through_table }.#{ source_foreign_key }
+        WHERE
+          #{ through_table }.#{ through_primary_key } = ?
+      SQL
+
+      source_class.parse_all(results).first
     end
   end
 end
